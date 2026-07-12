748ac9c0e1bb9241616e373e465ffe7bd004e4e6
Rockmeister56 <megacake.records@gmail.com>
2026-07-11 20:15:28 -0700

    Update bridge-cash-callahan-attorney-at-law.js

--- cash-callahan-pi/bridge-cash-callahan-attorney-at-law.js
+++ cash-callahan-pi/bridge-cash-callahan-attorney-at-law.js
@@ -195,10 +195,10 @@
            override the base rules above at this screen width, without touching mobile */
         @media (min-width: 470px) {
             .splash-avatar-container lemon-slice-widget {
-                transform: translate(calc(-50% - 25px), -50%);
+                transform: translate(calc(-50% - 10px), -50%);
             }
             .splash-service-line {
-                margin-top: 55px;
+                margin-top: 65px;
             }
         }
     `;
